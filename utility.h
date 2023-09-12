5bd8e0ff179bb2ed8f7c5b44883b8d442bb9f4dc
Anthony Coipel <99366232+Coipel@users.noreply.github.com>
2023-09-11 18:39:23 -0700

    fixed spelling mistake in utility.h

--- utility.h
+++ utility.h
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 
 /*
-  After decifing my notes it seems that aux1 (T1) = refund, aux2 (T2) = quarter, aux3 (T3) = dollar input (except it can vary between $1 & $5 
+  After deciphering my old notes it seems that aux1 (T1) = refund, aux2 (T2) = quarter, aux3 (T3) = dollar input (except it can vary between $1 & $5 
   based on external none button input) (T1-T2-T3 are going left to right after item buttons) I will add an aux4 to have aux3 be for $1 and aux4
   for $5. Note it was expected that a hidden input of a scanner would determine what to do after pushing the original dollar button and it would 
   have decided if it were $1, $5, or invalid and respond accordingly. With the above change there is no longer an invalid state or hidden input.
